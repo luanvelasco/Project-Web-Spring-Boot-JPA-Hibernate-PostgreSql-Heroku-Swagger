f6dd8161cbe4af7ff045681b5c637e81ab643793
Luan Velasco <velascoluan@gmail.com>
2021-06-17 17:29:14 -0300

    Fix pom.xml + Added JPA notation in User entity

--- src/main/java/com/lvm/workshops/entities/User.java
+++ src/main/java/com/lvm/workshops/entities/User.java
@@ -2,10 +2,15 @@ package com.lvm.workshops.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
  
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private Long id;
 	private String name;
 	private String email;
