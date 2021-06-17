2780946eddccff1259bb60fbbbce99fa61f2cdf1
Luan Velasco <velascoluan@gmail.com>
2021-06-17 17:35:45 -0300

    Added auto incrementation to @Id in User Entity

--- src/main/java/com/lvm/workshops/entities/User.java
+++ src/main/java/com/lvm/workshops/entities/User.java
@@ -3,6 +3,8 @@ package com.lvm.workshops.entities;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -11,6 +13,7 @@ public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
