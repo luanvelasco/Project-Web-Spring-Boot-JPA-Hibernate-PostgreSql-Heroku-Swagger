95d894b6273c1dcdce491680ca497ae193499b6c
Luan Velasco <velascoluan@gmail.com>
2021-06-17 12:32:26 -0300

    Start Project

---
+++ src/test/java/com/lvm/workshops/WorkshopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvm.workshops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
