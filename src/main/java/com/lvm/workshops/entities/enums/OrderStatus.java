f4cd35190029a8730a19ce41691e517fa0c1e93b
Luan Velasco <velascoluan@gmail.com>
2021-08-04 10:46:59 -0300

    Created OrderStatus and ServiceStatus enumerators

---
+++ src/main/java/com/lvm/workshops/entities/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.lvm.workshops.entities.enums;
+
+public enum OrderStatus {
+
+	WAITING_PAYMENT,
+	PAID,
+	CANCELED;
+}
---
+++ src/main/java/com/lvm/workshops/entities/enums/ServiceStatus.java
@@ -0,0 +1,8 @@
+package com.lvm.workshops.entities.enums;
+
+public enum ServiceStatus {
+
+	ON_GOING,
+	FINISHED,
+	CANCELED;
+}
